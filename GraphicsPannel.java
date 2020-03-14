2bbb31a4979421d73c5c254f24cc88dc8b1ea288
Carter Kosturos <tabernacle800@gmail.com>
2020-03-14 12:10:32 -0700

    Compile in main runner

--- GraphicsPannel.java
+++ GraphicsPannel.java
@@ -1,4 +1,14 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JFrame;
 import java.util.Random;
 import javax.swing.JPanel;
@@ -16,54 +26,8 @@ public class GraphicsPannel extends JPanel
    	 */
    	public void paintComponent (Graphics page)
 	{
-      	final int MID = 150+20;
-      	final int TOP = 50;
-      	
-      	page.setColor(Color.CYAN);
-      	page.fillRect(0, 0, getWidth(), getHeight());
-      	
-      	Color groundColor = new Color(127, 0, 64);
-
-    	page.setColor (groundColor);
-      	page.fillRect (0, 175, 300, 50);  // ground
-
-      	page.setColor (Color.yellow);
-      	page.fillOval (255,-40, 80, 80); // sun 
-
-      	page.setColor (Color.white);
-      	page.fillOval (MID - 20, TOP, 40, 40);      // head
-      	page.fillOval (MID - 35, TOP + 35, 70, 50); // upper torso
-      	page.fillOval (MID - 50, TOP + 80, 100, 60);  // lower torso
-      	
-      	//Buttons
-      	page.setColor (Color.black);
-      	page.fillOval(MID - 5, TOP + 40, 10, 10);
-      	page.fillOval(MID - 5, TOP + 60, 10, 10);
-
-      	page.setColor (Color.black);
-      	page.fillOval (MID - 10, TOP + 10, 5, 5);   // left eye
-      	page.fillOval (MID + 5, TOP + 10, 5, 5);    // right eye
-
-      	page.drawArc (MID - 10, TOP + 20, 20, 10, 5, 180);   // smile (Now frown)
-
-      	page.drawLine (MID - 25, TOP + 60, MID - 50, TOP + 40);  // left arm
-      	page.drawLine (MID + 25, TOP + 60, MID + 55, TOP + 60);  // right arm
-
-      	page.drawLine (MID - 20, TOP + 5, MID + 20, TOP + 5);  // brim of hat
-      	page.fillRect (MID - 15, TOP - 20, 30, 25);        // top of hat
-      	
-      	//My Additions
-      	//Makes Snow
-      	
-      	int snows = 0;
-      	Random rand = new Random();
-      	page.setColor (Color.white);
-      	while(snows<=100){
-      		int x_set = rand.nextInt(300);
-      		int y_set = rand.nextInt(300);
-      		page.fillOval(x_set,y_set,3,3);
-      		snows++;
-      	}
+      	final int MID = 500;
+      	final int TOP = 500;
    	}
    	
 	
---
+++ MainRunner.java
@@ -0,0 +1,24 @@
+/*
+Run Like:
+otherfile.main(args);
+*/
+
+class MainRunner{
+
+
+    public static void main (String[] args)
+	{
+
+        System.out.println("Boot!");
+
+        ButtonPannel.main(args);
+        GraphicsPannel.main(args);
+
+
+    }
+
+
+
+
+
+}
